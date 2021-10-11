53aaf38e9f7b551deb3ed1f5f8b05096550c55dc
Mustafa KARACABEY <mustafa.karacabey.engineer@gmail.com>
2021-10-11 11:10:19 +0300

    [Added-image-and-gridle-library]

--- app/src/main/java/com/example/weighbridge_on_universe/MainActivity.kt
+++ app/src/main/java/com/example/weighbridge_on_universe/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
 import android.widget.CheckBox
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -19,6 +20,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide.with(this).load(R.drawable.title).into(imageView)//Glide ile yükledik
+
 
         checkBoxMars.setOnClickListener(this) //Neleri Dinleyeceğini söyledik
         checkBoxJupi.setOnClickListener(this)
