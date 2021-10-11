0c1fc826701722fac72c5c86876b5825c10d8744
Mustafa KARACABEY <mustafa.karacabey.engineer@gmail.com>
2021-10-11 10:33:25 +0300

    [Added-Functionality-in-Mainlayout]

--- app/src/main/java/com/example/weighbridge_on_universe/MainActivity.kt
+++ app/src/main/java/com/example/weighbridge_on_universe/MainActivity.kt
@@ -2,28 +2,27 @@ package com.example.weighbridge_on_universe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.CheckBox
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val KG_TO_POUND: Double = 2.2045
     private val POUND_TO_KG : Double = 0.45359237
-    val MARS_CONSTANT : Double = 0.38
+    private val MARS : Double = 0.38
+    private val JUPITER : Double = 2.34
+    private val VENUS : Double = 0.91
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var userKilogram = inputKiloText.text
-
-        button_hesapla.setOnClickListener({ v ->
-            var poundKilo = convertKiloToPound(userKilogram.toString().toDouble())
-            var marsPound = poundKilo * MARS_CONSTANT
-            var marsKilo = convertPoundToKilo(marsPound)
-            result_view.text = marsKilo.toString()
-        })
-
+        checkBoxMars.setOnClickListener(this) //Neleri Dinleyeceğini söyledik
+        checkBoxJupi.setOnClickListener(this)
+        checkBoxVenus.setOnClickListener(this)
     }
 
     fun convertKiloToPound(kilo : Double) : Double {
@@ -36,5 +35,75 @@ class MainActivity : AppCompatActivity() {
 
     fun Double.Format(kacRakam : Int) = java.lang.String.format("%.${kacRakam}", this)
 
+    override fun onClick(v: View?) {//CheckBox Dinlicek şekilde configure ettim
+
+        v as CheckBox
+
+        var isChecked : Boolean = v.isChecked
+
+        var userKilogram = inputKiloText.text.toString().toDouble()
+        var pound = convertKiloToPound(userKilogram)
+
+        if(!TextUtils.isEmpty(inputKiloText.text.toString())) {
+            when(v.id) {
+                R.id.checkBoxJupi-> if(isChecked) {
+                    checkBoxMars.isChecked = false
+                    checkBoxVenus.isChecked = false
+                    calculateUserKilo(pound, v)
+                }
+                R.id.checkBoxMars-> if(isChecked){
+                    checkBoxVenus.isChecked = false
+                    checkBoxJupi.isChecked = false
+                    calculateUserKilo(pound, v)
+                }
+                R.id.checkBoxVenus-> if(isChecked) {
+                    checkBoxJupi.isChecked = false
+                    checkBoxMars.isChecked = false
+                    calculateUserKilo(pound, v)
+                }
+                else -> {
+                    println("Nothing !!!")
+                }
+            }
+
+        }
+
+
+
+    }
+
+
+    fun calculateUserKilo(pound: Double, checkBox: CheckBox) {
+        var calculatedpound : Double
+        var calculatedkilo : Double
+
+        when(checkBox.id) {
+
+            R.id.checkBoxMars -> {
+                calculatedpound = pound * MARS
+                calculatedkilo= convertPoundToKilo(calculatedpound)
+                result_view.text = calculatedkilo.toString()
+            }
+            R.id.checkBoxJupi -> {
+                calculatedpound = pound * JUPITER
+                calculatedkilo= convertPoundToKilo(calculatedpound)
+                result_view.text = calculatedkilo.toString()
+            }
+            R.id.checkBoxVenus -> {
+                calculatedpound = pound * VENUS
+                calculatedkilo= convertPoundToKilo(calculatedpound)
+                result_view.text = calculatedkilo.toString()
+            }
+        }
+    }
 
 }
+
+
+
+/*button_hesapla.setOnClickListener({ v ->
+          var poundKilo = convertKiloToPound(userKilogram.toString().toDouble())
+          var marsPound = poundKilo * MARS
+          var marsKilo = convertPoundToKilo(marsPound)
+          result_view.text = marsKilo.toString()
+      })*/
